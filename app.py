2b0d58c0dddcff7aed6114d5e7673115ba521372
Shubh9165 <shubhpatel915@gmail.com>
2026-04-13 09:12:17 +0530

    name changed

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain_core.prompts import PromptTemplate
 
 
 
-st.title("🔎 LangChain - Chat with search")
+st.title("🔎 SearchGPT Agent  ")
 """
 In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
 Try more LangChain 🤝 Streamlit Agent examples at [github.com/shubh9165/SearchGPT-Agent](https://github.com/shubh9165/SearchGPT-Agent).
